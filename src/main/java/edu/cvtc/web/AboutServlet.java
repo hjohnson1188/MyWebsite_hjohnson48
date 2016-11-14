70df20490cce7462e0a2854694a6dbc3be2ed0cc
Heather Johnson <heather@johnsonch.com>
2016-11-14 13:26:51 -0600

    initial commit

---
+++ src/main/java/edu/cvtc/web/AboutServlet.java
@@ -0,0 +1,40 @@
+package edu.cvtc.web;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AboutServlet
+ */
+@WebServlet("/AboutServlet")
+public class AboutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("<html><head><title>My Webpage</title></head>");
+		response.getWriter().append("<body><h1>My Webpage</h1><p>This is my About Page</p1></body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
