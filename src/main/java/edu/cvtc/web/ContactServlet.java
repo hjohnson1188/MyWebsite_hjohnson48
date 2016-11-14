248df094f7cca73772ba9bfc794d7afa77c25492
Heather Johnson <heather@johnsonch.com>
2016-11-14 14:05:37 -0600

    updating classes

--- src/main/java/edu/cvtc/web/ContactServlet.java
+++ src/main/java/edu/cvtc/web/ContactServlet.java
@@ -17,17 +17,22 @@ public class ContactServlet extends HttpServlet {
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ContactServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+  
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("<html><head><title>My Website</title>");
+		response.getWriter().append("<style>body{text-align: center;background-color: #A1E1DB;}");
+		response.getWriter().append("header{font-family: Calibri, sans-serif;font-size: 24pt;color: #1efad2;background-color: #fa1eb4;border-style: outset;border-width: small;border-color: #db1a9a;border-radius: 25px;}");
+		response.getWriter().append("footer{font-family: Calibri, san-serif;border-style: outset;border-width: small;border-color: #db1a9a;position: fixed;bottom: 0;width: 100%;border-radius: 25px;}");
+		response.getWriter().append("ul{list-style-type: none;margin: 0;padding: 0;font-family: Calibri, sans-serif;font-size: 24pt;color: #AABEE4;border-radius: 25px;border-style: outset;border-width: small;border-color: #71C4BB;background-color: #469E95;position: relative;left: 80px;top: 145px;width: 150%}");
+		response.getWriter().append("li{display: inline;text-decoration: none;color: #AABEE4}</style></head>");
+		response.getWriter().append("<body><header><h1>Heather Johnson's Webpage</h1></header>");
+		//response.getWriter().append("<ul><li><a href="MyWebsiteHomeServlet.java">Home</a></li><li><a href="">Contact</a></li><li><a href="AboutServlet.java">About</a></li></ul><section>");
+		response.getWriter().append("<p> </p></section><footer><p>&copy; Heather Johnson 2016</p></footer></body></html>");
 	}
 
 	/**
